d97f0859ec5d3e919ea725f456a6e7c2e3191105
namig <guseinovnamig41@gmail.ru>
2020-03-18 14:20:02 +0300

    Next time someone asks you how to fix an infinite loop, remember this commit

--- gui/main.py
+++ gui/main.py
@@ -6,8 +6,9 @@ import csv
 import random
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QInputDialog, QMessageBox
-from PyQt5.QtGui import QPainter, QColor, QFont
+from PyQt5.QtGui import QPainter, QColor, QFont, QColor
 from ps_window import Ui_MainWindow
+import ps_themes
 import ps_board
 import ps_settings
 import ps_operation
@@ -20,10 +21,17 @@ class PUSH_SWAP_GUI(QtWidgets.QMainWindow):
         super(PUSH_SWAP_GUI, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.board = ps_board.StackBoard(self)
+
+        # def themes
+        self.CURRENT_THEME_WINDOW = ps_themes.BLUE_WINDOW 
+        self.CURRENT_THEME_FRAME  = ps_themes.BLUE_FRAME
+        self.CURRENT_THEME_BUTTON = ps_themes.BLUE_BUTTON
+        self.CURRENT_THEME_LINE   = ps_themes.BLUE_LINE
+        self.set_themes('QMainWindow')
 
         self.dlg = None
         self.file_path = ""
-        self.board = ps_board.StackBoard(self)
 
         self.ui.action.triggered.connect(self.open_file)
         self.ui.action_2.triggered.connect(self.save_file)
@@ -32,6 +40,23 @@ class PUSH_SWAP_GUI(QtWidgets.QMainWindow):
 
         self.ui.pushButton.clicked.connect(self.start)
         self.ui.pushButton_2.clicked.connect(self.close_push_swap)
+
+    def set_themes(self, name_window):
+        if name_window == 'QMainWindow':
+            p = self.board.palette()
+            p.setColor(self.backgroundRole(), QColor(*self.CURRENT_THEME_FRAME))
+            self.board.setPalette(p)
+
+            self.setStyleSheet(self.CURRENT_THEME_WINDOW % (name_window))
+            self.ui.pushButton.setStyleSheet(self.CURRENT_THEME_BUTTON)
+            self.ui.pushButton_2.setStyleSheet(self.CURRENT_THEME_BUTTON)
+        else:
+            self.dlg.setStyleSheet(self.CURRENT_THEME_WINDOW % (name_window))
+            self.dlg_ui.lineEdit.setStyleSheet(self.CURRENT_THEME_LINE)
+            self.dlg_ui.lineEdit_2.setStyleSheet(self.CURRENT_THEME_LINE)
+            self.dlg_ui.lineEdit_3.setStyleSheet(self.CURRENT_THEME_LINE)
+            self.dlg_ui.pushButton.setStyleSheet(self.CURRENT_THEME_BUTTON)
+            self.dlg_ui.pushButton_2.setStyleSheet(self.CURRENT_THEME_BUTTON)
 
     def open_file(self):
         self.file_path = QFileDialog.getOpenFileName(self, 'Открыть', '/home')[0]
@@ -68,6 +93,7 @@ class PUSH_SWAP_GUI(QtWidgets.QMainWindow):
         self.dlg = QtWidgets.QDialog()
         self.dlg_ui = ps_settings.Ui_Dialog()
         self.dlg_ui.setupUi(self.dlg)
+        self.set_themes('QDialog')
         self.dlg.show()
 
         self.dlg_ui.pushButton.clicked.connect(self.get_settings)
---
+++ gui/ps_themes.py
@@ -0,0 +1,38 @@
+# DARK
+DARK_WINDOW = "%s{background-color: rgb(71, 71, 71); border: 1px solid black;}" 
+DARK_FRAME  = (43, 35, 35) 
+
+DARK_BUTTON  = "border-color: rgb(46, 52, 54);\n"
+DARK_BUTTON += "color: rgb(255, 255, 255);\n"
+DARK_BUTTON += "background-color: rgb(46, 52, 54);\n"
+DARK_BUTTON += "alternate-background-color: rgb(136, 138, 133);"
+
+DARK_LINE    = "background-color: rgb(240, 240, 240); \n"
+DARK_LINE   += "border: none;\n"
+DARK_LINE   += "border-bottom: 2px solid black;"
+
+# GREEN
+GREEN_WINDOW = "%s{background-color: rgb(4, 71, 22); border: 2px solid black;}" 
+GREEN_FRAME  = (4, 130, 38) 
+
+GREEN_BUTTON  = "border-color: rgb(1, 80, 40);\n"
+GREEN_BUTTON += "color: rgb(255, 255, 255);\n"
+GREEN_BUTTON += "background-color: rgb(1, 80, 40);\n"
+GREEN_BUTTON += "alternate-background-color: rgb(136, 138, 133);"
+
+GREEN_LINE    = "background-color: rgb(240, 240, 240); \n"
+GREEN_LINE   += "border: none;\n"
+GREEN_LINE   += "border-bottom: 2px solid black;"
+
+# BLUE
+BLUE_WINDOW = "%s{background-color: rgb(0, 31, 99); border: 2px solid black;}"
+BLUE_FRAME  = (0, 46, 148) 
+
+BLUE_BUTTON  = "border-color: rgb(16, 41, 97);\n"
+BLUE_BUTTON += "color: rgb(255, 255, 255);\n"
+BLUE_BUTTON += "background-color: rgb(16, 41, 97);\n"
+BLUE_BUTTON += "alternate-background-color: rgb(136, 138, 133);"
+
+BLUE_LINE    = "background-color: rgb(240, 240, 240); \n"
+BLUE_LINE   += "border: none;\n"
+BLUE_LINE   += "border-bottom: 2px solid black;"
